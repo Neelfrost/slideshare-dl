95f4d1bd40f8bb0dbf946c8e274db92d1a267297
Neel Basak <neelfrost@gmail.com>
2021-09-28 13:54:01 +0530

    feat: parallelize slide download

--- slideshare-dl.py
+++ slideshare-dl.py
@@ -1,4 +1,5 @@
 import argparse
+from concurrent.futures import ThreadPoolExecutor
 import glob
 import os
 import shutil
@@ -35,6 +36,13 @@ def create_parser():  # {{{
     return parser.parse_args()  # }}}
 
 
+def download_slide(idx, image_url, image_path):  # {{{
+    # Print slide being downloaded
+    print(f"Downloading slide: {idx}", end="\r")
+    # Download slide, save it in "temp" folder
+    return os.system(f"curl.exe -s {image_url} -o {image_path}")  # }}}
+
+
 def download_slides(url):  # {{{
     # Check if url if of slideshare
     # Exit if not
@@ -45,6 +53,7 @@ def download_slides(url):  # {{{
     page = requests.get(url)
     soup = BeautifulSoup(page.content, "html.parser")
     images = soup.find_all("img", class_="slide_image")
+    no_of_images = len(images)
 
     # Make "temp" dir in cwd
     if not os.path.isdir(TEMP_DIR):
@@ -59,22 +68,24 @@ def download_slides(url):  # {{{
     else:
         exit("No slides were found...")
 
-    # Save slides in the "temp" dir
-    for idx, image in enumerate(images, start=1):
-        image_url = image.get(res).split("?")[0]
-        image_name = image_url.split("/")[-1]
-        image_path = os.path.join("temp", image_name)
-        if os.path.isfile(image_path):
-            print(f"Slide: {idx} exists\r", end="")
-        else:
-            print(f"Downloading slide: {idx}\r", end="")
-            os.system(f"curl.exe -s {image_url} -o {image_path}")  # }}}
+    # Parallelize slide downloading
+    with ThreadPoolExecutor() as executor:
+        for idx, image in enumerate(images, start=1):
+            image_url = image.get(res).split("?")[0]
+            # Format image name to include slide index (with leading zeros)
+            image_name = (
+                str(idx).zfill(no_of_images // 100) + "-" + image_url.split("/")[-1]
+            )
+            image_path = os.path.join("temp", image_name)
+            if os.path.isfile(image_path):
+                print(f"Slide: {idx} exists", end="\r")
+            else:
+                executor.submit(download_slide, idx, image_url, image_path)  # }}}
 
 
 def convert_to_pdf(pdf_name, no_pdf=False):  # {{{
-    # Get all slides sorted by time
+    # Get all slides sorted by name
     files = glob.glob("temp/*")
-    files.sort(key=os.path.getmtime)
 
     if not no_pdf:
         print("Generating pdf...\r", end="", flush=True)
